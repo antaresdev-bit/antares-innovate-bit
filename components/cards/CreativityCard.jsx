7c7922537fe1266ff189cfbf496f65fe61a97ffc
andresgrondona <afgo4ster@gmail.com>
2025-03-02 17:17:25 -0500

    cards landing

--- components/cards/CreativityCard.jsx
+++ components/cards/CreativityCard.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 function CreativityCard() {
   return (
-    <div className="relative mx-5 md:mx-[100px] mb-12 p-[1px] bg-transparent rounded-[48px] border border-white">
-      <div className="flex flex-wrap lg:flex-nowrap justify-between items-center bg-transparent rounded-[24px] p-8 md:p-[32px] text-white">
-        <div className="w-full md:w-full lg:w-1/2 flex justify-center order-first lg:order-last mb-4 md:mb-4 lg:mb-0">
+    <div className="relative mx-[21px] sm:mx-[21px] md:mx-[49px] lg:mx-[71px] mb-12 p-[1px] bg-transparent rounded-[48px] border border-blue-500">
+      <div className="flex flex-wrap lg:flex-nowrap justify-between items-center bg-transparent rounded-[24px] p-[15px] sm:p-[15px] md:p-[22px] lg:p-[32px] text-white">
+       
+      <div className="w-full md:w-full lg:w-auto flex justify-center items-center ">
           <div className="relative">
-            {/* Imagen para mobile */}
             <Image
               src="/assets/images/portadas-servicios-home/creatividad-portada-5.png"
               alt="Creatividad"
@@ -15,41 +15,37 @@ function CreativityCard() {
               height={400}
               className="rounded-lg md:hidden"
             />
-            {/* Imagen para tablet (md) */}
             <Image
               src="/assets/images/portadas-servicios-home/creatividad-portada-6.png"
               alt="Creatividad Tablet"
-              width={400}
+              width={1000}
               height={400}
-              className="rounded-lg hidden md:block lg:hidden"
+              className="rounded-lg hidden md:block lg:hidden mb-0 "
             />
-            {/* Imagen para desktop (lg) */}
             <Image
               src="/assets/images/portadas-servicios-home/creatividad-portada-5.png"
               alt="Creatividad Desktop"
-              width={400}
+              width={600}
               height={400}
               className="rounded-lg hidden md:hidden lg:block"
             />
           </div>
         </div>
 
-        <div className="w-full md:w-full lg:w-1/2 border border-white rounded-[48px] h-auto md:h-[396px] flex flex-col justify-center">
-          <div className=" ">
+        {/* Contenedor rojo con altura automática */}
+        <div className="w-full md:w-full lg:flex-grow border border-red-500 rounded-[24px] flex flex-col justify-between p-[32px] min-h-full">
+          <div>
             <h2
-              className="font-bold mb-[37px] ml-10 "
+              className="font-bold mb-[37px]"
               style={{ fontFamily: "HandelGothic", fontSize: "40px" }}
             >
               Creatividad
             </h2>
-            <p
-              className=" ml-5  "
-              style={{ fontFamily: "UniteaSans", fontSize: "18px" }}
-            >
+            <p style={{ fontFamily: "UniteaSans", fontSize: "18px" }}>
               Transformamos ideas en experiencias visuales de alto impacto:
             </p>
             <ul
-              className="list-disc pl-5 lg:mb-[45px] lg:mb-[0px]  sm:mb-[22px] space-y-2 ml-5"
+              className="mt-[10px] ml-[20px]"
               style={{ fontFamily: "UniteaSans", fontSize: "18px" }}
             >
               <li>
@@ -64,13 +60,13 @@ function CreativityCard() {
               </li>
             </ul>
           </div>
-          <div className=" mb-[22px]  sm:mb-[22px] flex items-center">
+          <div className="flex items-center mt-6">
             <Image
-              src="/assets/images/Boton avion animado 2.gif"
+              src="/assets/images/Gif Avion.gif"
               alt="Botón Animado"
-              width={80}
-              height={80}
-              className="mr-2" 
+              width={48}
+              height={48}
+              className=""
             />
             <button className="w-[230px] h-[48px] rounded-[32px] bg-white text-black font-bold flex items-center justify-center">
               Ver Portafolio
