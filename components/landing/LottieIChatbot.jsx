401130c09e6407742a303b8df98c291b585cc7fa
andresgrondona <afgo4ster@gmail.com>
2025-03-07 09:55:03 -0500

    whatssap social networks

--- components/landing/LottieIChatbot.jsx
+++ components/landing/LottieIChatbot.jsx
@@ -1,9 +1,18 @@
+"use client"
+
 import React from "react";
 
 function LottieIChatbot() {
+  const handleClick = () => {
+    window.open("https://wa.me/573053456611", "_blank");
+  };
+
   return (
     <div className="fixed bottom-[25px] right-[21px] sm:right-[21px] md:right-[49px] lg:right-[71px] z-[1000]">
-      <button className="w-[98px] h-[104px] border-none bg-transparent cursor-pointer p-0 relative">
+      <button
+        onClick={handleClick}
+        className="w-[98px] h-[104px] border-none bg-transparent cursor-pointer p-0 relative"
+      >
         <img
           src="/assets/images/lottiChatbot.gif"
           alt="Chatbot Animation"
