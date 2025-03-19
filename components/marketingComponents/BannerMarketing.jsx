26fa988b1aadbf3270a0f9d545c44d827b7eae67
andresgrondona <afgo4ster@gmail.com>
2025-03-19 11:14:28 -0500

    marketing banner

--- components/marketingComponents/BannerMarketing.jsx
+++ components/marketingComponents/BannerMarketing.jsx
@@ -2,11 +2,32 @@ import Image from "next/image";
 
 const BannerMarketing = () => {
   return (
-    <div className="w-full max-w-[1500px] mx-auto relative overflow-hidden ">
+    <div className="w-full max-w-[1500px] mx-auto relative overflow-hidden">
       <div
-        className="relative w-full h-[457px] bg-gradient-to-r from-[#0046DC] to-[#4D86FF] flex sm:items-center sm:justify-between justify-center px-4 sm:px-10 overflow-hidden"
+        className="relative w-full h-[457px] flex sm:items-center sm:justify-between justify-center px-4 sm:px-10 overflow-hidden"
         style={{ borderRadius: "0 0 48px 48px" }}
       >
+        <div className="absolute inset-0 z-0">
+          <Image
+            src="/assets/images/marketing/Banner1.png"
+            alt="Banner"
+            fill
+            className="hidden lg:block object-cover"
+          />
+          <Image
+            src="/assets/images/marketing/Banner2.png"
+            alt="Banner"
+            fill
+            className="hidden md:block lg:hidden object-cover"
+          />
+          <Image
+            src="/assets/images/marketing/Banner3.png"
+            alt="Banner"
+            fill
+            className="block md:hidden object-cover"
+          />
+        </div>
+
         <div className="hidden sm:flex flex-col space-y-2 sm:space-y-4 text-left z-10 max-w-[50%] lg:ml-[140px] lg:w-[600px] mt-[100px]">
           <h1
             className="text-3xl sm:text-5xl font-bold text-white md:text-[46px] lg:text-[65px] mb-[30px]"
@@ -15,10 +36,10 @@ const BannerMarketing = () => {
             Industria del Marketing
           </h1>
 
-          <div className="flex items-center space-x-4  ">
+          <div className="flex items-center space-x-4">
             <div
               className="flex items-center justify-center rounded-full bg-white"
-              style={{ width: "65px", height: "40px" }}
+              style={{ width: "55px", height: "55px", aspectRatio: "1 / 1" }}
             >
               <Image
                 src="/assets/images/marketing/Vector marketing.png"
@@ -36,19 +57,6 @@ const BannerMarketing = () => {
               En Antares, no solo anticipamos la revolución digital; la
               lideramos.
             </p>
-          </div>
-        </div>
-
-        <div className="relative flex items-center justify-center min-w-[480px] h-full  max-w-[100%] sm:max-w-[100%] lg:max-w-[35%] z-10 sm:-mr-10 mx-auto lg:mr-[100px]">
-          <div className="w-full h-full flex items-center justify-center relative">
-            <Image
-              src="/assets/images/marketing/fondo-objetivo-realista 1.png"
-              alt="Industria Inmobiliaria"
-              width={500}
-              height={500}
-              className="object-contain w-full h-full absolute bottom-[-48px]"
-              priority
-            />
           </div>
         </div>
       </div>
