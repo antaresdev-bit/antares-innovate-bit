fd93af7ed96ac19c668eda6d7f17718a0128dfdb
Michelle <cadavidcarmona1@gmail.com>
2025-03-18 22:12:21 -0500

    Optimización carga rápida portfolio

---
+++ app/[locale]/(content)/portafolio/default.js
@@ -0,0 +1,24 @@
+export default function Default() {
+  return (
+    <div className="relative bg-opacity-70">
+      <div
+        className="absolute inset-x-0 mx-auto w-full max-w-[1409px] h-[542px]"
+        style={{
+          background: "radial-gradient(ellipse at center, #334385 0%, #0E051C 60%)",
+          transform: "translateY(-70%)",
+          zIndex: -1,
+        }}
+      />
+      <div className="max-w-[1500px] mx-auto">
+        <div className="mt-[187px] mx-[21px] sm:mx-[21px] md:mx-[49px] lg:mx-[71px]">
+          <h1
+            className="mb-[69px] ml-[0px] sm:ml-[0px] md:ml-[65px] lg:ml-[68px] text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#4D86FF] to-[#FFFFFF] leading-[65px] max-w-[400px]"
+            style={{ fontFamily: "HandelGothic" }}
+          >
+            portafolio
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+} 
