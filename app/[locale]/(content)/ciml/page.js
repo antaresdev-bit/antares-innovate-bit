7befc91e94fac901a3df987430b4ede3bdcbc18b
andresgrondona <afgo4ster@gmail.com>
2025-03-23 10:13:26 -0500

    ciml subpage en es

--- app/[locale]/(content)/ciml/page.js
+++ app/[locale]/(content)/ciml/page.js
@@ -6,16 +6,18 @@ import ConditionalText from "@/components/subPages/ConditionalTextResponsive";
 import GridImageSection from "@/components/subPages/GridImageSection";
 import ProcessText from "@/components/subPages/ProcessText";
 import TextSubPages from "@/components/subPages/TextSubPages";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 function CimlPage() {
+  const t = useTranslations("cimlSubpage");
   return (
     <div className="relative">
       <div>
         <BannerTest
           videoUrl="https://storage.googleapis.com/antares-agency-rcs/ciml%20Mockup%20Mo%CC%81dulo%20w.mp4"
           title="CIML"
-          subtitle="Innovación Editorial para la Salud Pública"
+          subtitle={t("cimlText2")}
           textPosition="left"
           ColorText="linear-gradient(to right, #862AA5, #FFFFFF)"
         />
@@ -36,15 +38,15 @@ function CimlPage() {
 
         <ConditionalText
           title="CIML"
-          subtitle="Innovación Editorial para la Salud Pública"
-          gradient="linear-gradient(to right, #DA6A1A, #FFFFFF)"
+          subtitle={t("cimlText1")}
+          gradient={t("cimlText2")}
         />
 
         <div className=" mt-[113px] sm:mt-[113px] md:mt-[119px] lg:mt-[152px] mb-[113px] sm:mb-[111px] md:mb-[111px] lg:mb-[154px]  ">
           <TextSubPages
-            title="Estrategias Visuales para la Educación y Concienciación Social"
-            subtitle="Editorial | Redes Sociales | Manuales | Impreso/Digital "
-            paragraph="El Centro de Innovación Mujer Latina (CIML) ha sido un pilar en la generación de contenido educativo y social en Estados Unidos. Nuestra colaboración se enfocó en el desarrollo de materiales editoriales, estructurando manuales, libros, cartillas, brochures y revistas en formatos físicos y digitales."
+            title={t("cimlText2")}
+            subtitle={t("cimlText3")}
+            paragraph={t("cimlText4")}
           />
         </div>
 
@@ -72,9 +74,9 @@ function CimlPage() {
         <div className="mt-[108px] sm:mt-[108px] md:mt-[111px] lg:mt-[156px] mb-[100px] sm:mb-[100px] md:mb-[106px] lg:mb-[155px]  ">
           <ProcessText
             paragraphs={[
-              "Uno de los proyectos más relevantes fue la campaña impulsada por el Gobierno de los Estados Unidos, destinada a la prevención y tratamiento de la crisis de opioides. Este proyecto incluyó la creación de documentos instructivos dirigidos a diferentes sectores: niños y jóvenes, adultos en general, profesionales de la salud (con énfasis en psicología) y padres de familia.",
-              "Para esta iniciativa, se desarrolló un diseño editorial con un tratamiento gráfico especializado, asegurando que el mensaje transmitiera con precisión los aspectos médicos, psicológicos y sociales de la problemática. La identidad visual fue cuidadosamente elaborada para reflejar seriedad y credibilidad, utilizando elementos gráficos que facilitaran la comprensión de la información según el público objetivo.",
-              "Cada pieza editorial combinó diagramación estructurada, iconografía explicativa y contenido visual de apoyo, garantizando una lectura intuitiva y efectiva. Este proyecto consolidó la capacidad de CIML para impulsar campañas de impacto social y reafirmó nuestra experiencia en diseño y producción editorial especializada en el sector de salud pública.",
+              t("cimlText5"),
+              t("cimlText6"),
+              t("cimlText7")
             ]}
           />
         </div>
