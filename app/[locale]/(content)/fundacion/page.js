8348e9b21027f27678f580965d7811090c664722
andresgrondona <afgo4ster@gmail.com>
2025-03-23 10:59:24 -0500

    fundacionHeinrich Subpage en es

--- app/[locale]/(content)/fundacion/page.js
+++ app/[locale]/(content)/fundacion/page.js
@@ -7,16 +7,18 @@ import BannerTest from "@/components/subPages/BannerSubPages";
 import ConditionalText from "@/components/subPages/ConditionalTextResponsive";
 import ProcessText from "@/components/subPages/ProcessText";
 import TextSubPages from "@/components/subPages/TextSubPages";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 function FundacionPage() {
+  const t = useTranslations("fundacionHeinrichSubpage");
   return (
     <div className="relative">
       <div>
         <BannerTest
           videoUrl="https://storage.googleapis.com/antares-agency-rcs/fundacion6.mp4"
-          title="Fundación Heinrich Böll"
-          subtitle="Escuela de Administración y Gestión del Estado para Transiciones Justas"
+          title={t("fundacionHeinText1")}
+          subtitle={t("fundacionHeinText2")}
           textPosition="left"
           ColorText="linear-gradient(to right, #91C746, #FFFFFF)"
         />
@@ -36,16 +38,16 @@ function FundacionPage() {
         {/* degrade */}
 
         <ConditionalText
-          title="Fundación Heinrich Böll"
-          subtitle="Escuela de Administración y Gestión del Estado para Transiciones Justas"
+          title={t("fundacionHeinText1")}
+          subtitle={t("fundacionHeinText2")}
           gradient="linear-gradient(to right, #91C746, #FFFFFF)"
         />
 
         <div className=" mt-[113px] sm:mt-[113px] md:mt-[119px] lg:mt-[152px] mb-[113px] sm:mb-[111px] md:mb-[111px] lg:mb-[154px]  ">
           <TextSubPages
-            title="Narrativas Visuales para el Cambio: Estrategias de Transición Justa"
-            subtitle="Producción Audiovisual | Motion Graphics"
-            paragraph="Colaboramos en la producción audiovisual documental de la Escuela de Administración y Gestión del Estado para Transiciones Justas, un proyecto impulsado por la Fundación Heinrich Böll en Colombia."
+            title={t("fundacionHeinText3")}
+            subtitle={t("fundacionHeinText4")}
+            paragraph={t("fundacionHeinText5")}
           />
         </div>
 
@@ -54,9 +56,9 @@ function FundacionPage() {
         <div className="mt-[108px] sm:mt-[108px] md:mt-[111px] lg:mt-[156px] mb-[100px] sm:mb-[100px] md:mb-[106px] lg:mb-[155px]  ">
           <ProcessText
             paragraphs={[
-              "Este programa busca integrar a diversos actores, incluyendo tomadores de decisiones y políticos, en la adopción de estrategias que armonicen el desarrollo socioeconómico con la sostenibilidad ambiental.",
-              "El documental captura testimonios clave de profesores, estudiantes y expertos en el campo, mostrando el impacto de la iniciativa en la formación de nuevas políticas públicas alineadas con principios de justicia ambiental y climática.",
-              "A través de una estética visual 'orgánica', reflejamos la identidad ecológica y social de la fundación, utilizando una edición dinámica que combina entrevistas, fragmentos de clases y síntesis de los temas abordados. Este material audiovisual se convierte en una herramienta clave para promover la integración de políticas de transición justa, destacando el compromiso de la Fundación Heinrich Böll con el medio ambiente y la equidad social.",
+              t("fundacionHeinText6"),
+              t("fundacionHeinText7"),
+              t("fundacionHeinText8"),
             ]}
           />
         </div>
