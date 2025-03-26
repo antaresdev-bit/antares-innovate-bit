3b35080a1f11313df9e182301defbf0da26bfafd
Michelle <cadavidcarmona1@gmail.com>
2025-03-26 12:16:24 -0500

    general loading message

--- components/layout/Loading/Loading.jsx
+++ components/layout/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 import { useTranslations } from 'next-intl';
 
 export default function Loading() {
-    const t = useTranslations("landing");
+    const t = useTranslations("loading");
     return (
         <div className="absolute inset-0 flex items-center justify-center bg-[#0e051c]/50">
         <div className="flex flex-col items-center mt-[100px]">
