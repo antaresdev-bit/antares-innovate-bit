11d5208e7da175d8708956b3fc3c4e540c008674
andresgrondona <afgo4ster@gmail.com>
2025-03-31 09:30:43 -0500

    update card blog

--- components/blogComponents/CardBlog.jsx
+++ components/blogComponents/CardBlog.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useLocale } from "next-intl";
+import { useTranslations } from "next-intl";
 
 function CardBlog() {
+  const t = useTranslations("portafolio");
   const locale = useLocale();
   return (
     <div
-      className="mt-[100px] relative mb-12 p-[1px] bg-[#1C5DE9] lg:bg-[white] sm:bg-[#1C5DE9] md:bg-[#1C5DE9] lg:bg-none rounded-[24px] sm:rounded-[24px] md:rounded-[48px] lg:rounded-[48px] sm:pb-[0px] md:pb-[0px] lg:pt-[40px] "
+      className="relative mt-[120px]  sm:mt-[120px]  md:mt-[100px] lg:mt-[184px] mb-12 p-[1px] bg-[#1C5DE9] lg:bg-[white] sm:bg-[#1C5DE9] md:bg-[#1C5DE9] lg:bg-none rounded-[24px] sm:rounded-[24px] md:rounded-[48px] lg:rounded-[48px] sm:pb-[0px] md:pb-[0px] lg:pt-[40px] "
       style={{
         backgroundImage: "none",
       }}
@@ -23,27 +25,27 @@ function CardBlog() {
       ></div>
 
       <div className="flex flex-wrap lg:flex-nowrap justify-between items-center rounded-[24px] p-[15px] sm:p-[15px] md:p-[22px] lg:p-[32px] text-white relative z-10">
-        <div className="w-full md:w-full lg:w-auto flex justify-center order-first lg:order-last mb-4 md:mb-4 lg:mb-0">
+        <div className="w-full md:w-full lg:w-auto flex justify-center order-first lg:order-last mb-4 md:mb-4 lg:mb-0  ml-[0px] sm:ml-[0px] md:ml-[30px] ">
           <div className="relative">
             <Image
-              src="/assets/images/inmobiliaria/1.png"
-              alt="Tecnologia"
+              src="/assets/images/portadas-servicios-home/cardInfo.png"
+              alt="Consultoria"
               width={400}
               height={400}
               className="rounded-lg md:hidden"
             />
             <Image
-              src="/assets/images/inmobiliaria/2.png"
-              alt="Tecnologia Tablet"
+              src="/assets/images/portadas-servicios-home/cardInfotablet.png"
+              alt="Consultoria Tablet"
               width={700}
               height={400}
               className="rounded-lg hidden md:block lg:hidden"
             />
             <Image
-              src="/assets/images/inmobiliaria/1.png"
+              src="/assets/images/portadas-servicios-home/cardInfo.png"
               alt="Tecnologia Desktop"
-              width={700}
-              height={600}
+              width={560}
+              height={300}
               className="rounded-lg hidden md:hidden lg:block"
             />
           </div>
@@ -59,28 +61,28 @@ function CardBlog() {
                 color: "white",
               }}
             >
-              ¿Hacemos algo increíble juntos?
+              {t("cardPortTittle")}
             </h2>
             <p
               style={{
                 fontFamily: "UniteaSans",
-               
-                color: "white",
-              }}
-             className="text-[17px] sm:text-[17px] md:text-[17px] lg:text-[22px]">
-              Has visto nuestro trabajo, ahora es tu turno. Si tienes un
-              proyecto en mente y quieres llevarlo al siguiente nivel, hablemos.
-            </p>
-            <ul
-              className="mt-[30px] ml-[40px] text-[17px] sm:text-[17px] md:text-[17px] lg:text-[22px]"
-              style={{
-                fontFamily: "UniteaSans",
+                fontSize: "18px",
                 color: "white",
               }}
             >
-              <li>Transformamos ideas en experiencias digitales</li>
-              <li>Fusionamos creatividad, tecnología y estrategia.</li>
-              <li>Potenciamos marcas con innovación y automatización.</li>
+              {t("cardPortSubTittle")}
+            </p>
+            <ul
+              className="mt-[10px] ml-[20px]"
+              style={{
+                fontFamily: "UniteaSans",
+                fontSize: "18px",
+                color: "white",
+              }}
+            >
+              <li>{t("cardPortInfo1")}</li>
+              <li>{t("cardPortInfo2")}</li>
+              <li>{t("cardPortInfo3")}</li>
             </ul>
           </div>
           <div className="flex items-center mt-10">
@@ -92,12 +94,12 @@ function CardBlog() {
               className=""
               unoptimized={true}
             />
-            <Link href={`/${locale}/form-contact`} className="w-full sm:w-auto">
+            <Link href={`/${locale}/portafolio`} className="w-full sm:w-auto">
               <button
                 className="w-full sm:w-[230px] h-[48px] bg-white text-[#02021E] text-[20px] rounded-[32px]  hover:bg-gray-200 transition duration-300 font-bold"
                 style={{ fontFamily: "HandelGothic" }}
               >
-                Contáctanos
+                {t("buttonPort")}
               </button>
             </Link>
           </div>
@@ -108,4 +110,7 @@ function CardBlog() {
 }
 
 export default CardBlog;
+
+
+
 
