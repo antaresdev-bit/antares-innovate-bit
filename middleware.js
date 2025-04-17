92eb90278df7260c8cddaf691f11392db3f9def7
andresgrondona <afgo4ster@gmail.com>
2025-04-17 15:56:44 -0500

    en version always

--- middleware.js
+++ middleware.js
@@ -1,15 +1,33 @@
+// import createMiddleware from 'next-intl/middleware';
+// import { locales } from './intlConfig';
+ 
+// const nextIntlMiddleware = createMiddleware({
+//   locales: locales,
+//   defaultLocale: 'en'
+// });
+
+// export default function(req){
+//   return nextIntlMiddleware(req)
+// }
+
+// export const config = {
+//   matcher: ['/', '/(en|es)/:path*']
+// };
+
 import createMiddleware from 'next-intl/middleware';
 import { locales } from './intlConfig';
- 
+
 const nextIntlMiddleware = createMiddleware({
-  locales: locales,
-  defaultLocale: 'en'
+  locales,
+  defaultLocale: 'en',
+  localeDetection: false 
 });
 
-export default function(req){
-  return nextIntlMiddleware(req)
+export default function middleware(req) {
+  return nextIntlMiddleware(req);
 }
 
 export const config = {
   matcher: ['/', '/(en|es)/:path*']
 };
+
