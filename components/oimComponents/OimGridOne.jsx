a47d6dc18c94a89e4c1ac44c9d99fe9a03fc180f
andresgrondona <afgo4ster@gmail.com>
2025-04-25 16:11:59 -0500

    oim images seo

--- components/oimComponents/OimGridOne.jsx
+++ components/oimComponents/OimGridOne.jsx
@@ -6,7 +6,7 @@ function OimGridOne() {
       <div className="w-full max-w-[1298px] mx-auto mb-[14px] sm:mb-[14px] md:mb-[23px] lg:mb-[25px] rounded-3xl overflow-hidden aspect-[1298/869] ">
         <img
           src="/assets/images/oim/5.jpg"
-          alt="ciml"
+          alt="Visual Mapping of Haitian Migration Patterns for U.S. Policy Analysis"
           className="w-full h-full object-cover"
         />
       </div>
@@ -14,7 +14,7 @@ function OimGridOne() {
       <div className="w-full max-w-[1298px] mx-auto mt-[14px] sm:mt-[14px] md:mt-[23px] lg:mt-[25px] rounded-3xl overflow-hidden aspect-video ">
         <img
           src="/assets/images/oim/6.jpg"
-          alt="ciml"
+          alt="Interactive Migration Data Visualization for Humanitarian Agencies"
           className="w-full h-full object-cover"
         />
       </div>
@@ -22,7 +22,7 @@ function OimGridOne() {
       <div className="w-full max-w-[1298px] mx-auto mt-[14px] sm:mt-[14px] md:mt-[23px] lg:mt-[25px] rounded-3xl overflow-hidden aspect-video ">
         <img
           src="/assets/images/oim/7.jpg"
-          alt="ciml"
+          alt="Migration Flow Visualizations Supporting U.S. Border Policy Decisions"
           className="w-full h-full object-cover"
         />
       </div>
