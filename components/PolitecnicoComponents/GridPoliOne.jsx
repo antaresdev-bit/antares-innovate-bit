d0a6d460a9613778a119ff2dc71ef4ee398b2529
andresgrondona <afgo4ster@gmail.com>
2025-04-25 16:04:20 -0500

    politecnico seo images

--- components/PolitecnicoComponents/GridPoliOne.jsx
+++ components/PolitecnicoComponents/GridPoliOne.jsx
@@ -7,14 +7,14 @@ function GridPoliOne() {
       <div className="w-full max-w-[1298px] mx-auto mb-[14px] sm:mb-[14px] md:mb-[23px] lg:mb-[25px] rounded-3xl overflow-hidden aspect-[1298/869]">
         <img
           src="/assets/images/politecnico/3.png"
-          alt="ciml"
+          alt="Innovative Digital Campaigns for American Youth Engagement"
           className="w-full h-full object-cover"
         />
       </div>
       <div className="w-full max-w-[1298px] mx-auto mb-[14px] sm:mb-[14px] md:mb-[23px] lg:mb-[25px] rounded-3xl overflow-hidden aspect-[1298/869]">
         <img
           src="/assets/images/politecnico/4.png"
-          alt="ciml"
+          alt="Digital Marketing Solutions for U.S. Audiences"
           className="w-full h-full object-cover"
         />
       </div>
